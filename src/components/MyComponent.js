c5e4379c3d7b2c15ec54dbac6004ccecf0960bde
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-06 23:49:55 +0700

    23 Props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,14 +2,20 @@
 // function component
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
     //JSX
     render() {
+        const myInfor = ['ab', 'c', 'c'];
         return (
             <div>
                 <UserInfor />
+                <br /> <br />
+                <DisplayInfor name="Hoi Dan IT" age="30" />
+                <hr />
+                <DisplayInfor name={"Eric"} age={true} myInfor={myInfor} />
             </div>
 
         );
