5a21641da3d28891f9a8c56f4ae1e208c5f73f74
tuhoc.cc <ksgoku2004@gmail.com>
2025-03-08 23:08:25 +0700

    33 React Lifecycle

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -4,15 +4,38 @@ import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
 
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log(">>> call constructor: 1")
+        super(props);
+        //babel compiler
+        this.state = {
+            isShowListUser: true
+        }
     }
+    componentDidMount() {
+        console.log('>>> call me components did mount')
+        setTimeout(() => {
+            document.title = 'Eric & Hoi Dan IT'
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapShot) {
+        console.log('>>> call me component did update', this.props, prevProps)
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 users')
+            }
+        }
+    }
+
+
     handleShowHide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
     render() {
+        console.log(">>> call me render")
         //destructing array/object
         const { listUsers } = this.props; //object
         // const listUsers = this.props.listUsers
@@ -21,7 +44,7 @@ class DisplayInfor extends React.Component {
         //template + logic js
         return (
             <div className="display-infor-container">
-                <img src={logo} />
+                {/* <img src={logo} /> */}
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Hide list users: " : " Show list users: "}
